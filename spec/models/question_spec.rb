4452c5256259387d3bb0c590a20060fb4ab4d2ab
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-20 23:13:44 -0700

    Assignment 28 completed. Added question and answer models and their respectice rspec attribute tests.

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+
+  describe "attributes" do
+    it "should respond to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(question).to respond_to(:body)
+    end
+  end
+end
