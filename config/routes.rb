ebf33790ee5f854ef06ea23a756cab0664b2a881
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-22 00:25:05 -0700

    Checkpoint 34 completed. nested posts within topics.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
-
   root to: 'welcome#index'
 end
