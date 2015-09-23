20e0c47ecf0a529d5a6e2bdade8739243d83bb4d
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-22 19:25:49 -0700

    Assignment 35 completed. Added validations for topic resource.

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
   it { should have_many(:posts) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:public) }
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
 
   describe "attributes" do
     it "should respond to name" do
