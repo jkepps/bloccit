ce157ad9d977ad007701b3953865db35625b520a
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-25 12:45:19 -0700

    Assignment 37 complete. Refactored code in users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    @user = User.new(user_params)
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
@@ -20,10 +16,11 @@ class UsersController < ApplicationController
   end
 
   def confirm
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    @user = User.new(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
