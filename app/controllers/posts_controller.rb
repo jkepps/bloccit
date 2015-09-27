206c553fe3df7756904e4e9d101359ca1fd3be77
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-27 13:56:35 -0700

    Checkpoint 44 completed. Added authorization components.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
+  before_action :authorize_user, except: [:show, :new, :create]
 
   def show
     @post = Post.find(params[:id])
@@ -56,5 +57,13 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def authorize_user
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin?
+      flash[:error] = "You must be an admin to do that."
+      redirect_to [post.topic, post]
+    end
   end
 end
