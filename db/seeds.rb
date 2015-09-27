c4c8fd44d5bf8b7045a0cc0904ceb294c9aa194d
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-27 13:56:35 -0700

    Checkpoint 44 completed. Added authorization components.

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,9 +38,18 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'jordan@seeku.co',
+# Create an admin user
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+# Create a member
+member = User.create!(
+  name: 'Member User',
+  email: 'member@example.com',
   password: 'helloworld'
 )
 
