df56af4d37444f0a8ded4d92af3abf5cd39bc578
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-27 00:26:17 -0700

    Checkpoint 44 in progress. Left off at 'roles in the PostsController'.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
   it { should have_many(:posts) }
+
   # Shoulda tests for name
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
@@ -29,6 +30,7 @@ RSpec.describe User, type: :model do
     end
   end
 
+
   describe "invalid user" do
     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
@@ -44,6 +46,48 @@ RSpec.describe User, type: :model do
 
     it "should be an invalid user due to incorrectly formmated email address" do
       expect(user_with_invalid_email_format).to_not be_valid
+    end
+
+    it "should respond to role" do
+      expect(user).to respond_to(:role)
+    end
+
+    it "should respond to admin?" do
+      expect(user).to respond_to(:admin?)
+    end
+
+    it "should resond to member?" do
+      expect(user).to respond_to(:member?)
+    end
+  end
+
+  describe "roles" do
+    it "should be member by default" do
+      expect(user.role).to eql("member")
+    end
+
+    context "member user" do
+      it "should return true for #member?" do
+        expect(user.member?).to be_truthy
+      end
+
+      it "should return false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+    end
+
+    context "admin user" do
+      before do
+        user.admin!
+      end
+
+      it "should return false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "should return true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
     end
   end
 end
