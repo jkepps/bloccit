26ae14001e7de8f1b89f45584bf79b013bce0841
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-27 22:34:00 -0700

    Checkpoint 45 completed. Added remaining methods for commen functionality.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe Comment, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
-  let(:comment) { Comment.create!(body: "Comment Body", post: post) }
+  let(:comment) { Comment.create!(body: "Comment Body", post: post, user: user) }
+
+  it { should belong_to(:post) }
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:body).is_at_least(5) }
 
   describe "attributes" do
     it "should respond to body" do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-  it { should have_many(:posts) }
 
+  it { should have_many(:posts) }
+  it { should have_many(:comments) }
+  
   # Shoulda tests for name
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
