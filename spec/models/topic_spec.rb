6b25db8dc7d464f6996f89736f2bc8e5090e86f8
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-30 22:38:56 -0700

    Checkpoint 50 completed. Scoped private topics and made them unavailable for viewing to guests.

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Topic, type: :model do
   it { should have_many(:labels).through(:labelings) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
-  it { should validate_presence_of(:public) }
+  #it { should validate_presence_of(:public) }
   it { should validate_length_of(:name).is_at_least(5) }
   it { should validate_length_of(:description).is_at_least(15) }
 
@@ -28,6 +28,25 @@ RSpec.describe Topic, type: :model do
 
     it "should be public by default" do
       expect(topic.public).to be(true)
+    end
+  end
+
+  describe "scopes" do
+    before do
+      @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic.update_attribute(:public, false)
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = User.new
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+
+      it "returns only public topics if user is nil" do
+        expect(Topic.visible_to(nil)).to eq([@public_topic])
+      end
     end
   end
 end
