37f5515815be0be23041e9689be98bb6cdb0d435
Jordan Epps <jordan.k.epps@gmail.com>
2015-09-30 22:38:56 -0700

    Checkpoint 50 completed. Scoped private topics and made them unavailable for viewing to guests.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,9 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
-  validates :public, presence: true
+  # validates :public, presence: true
 end
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -4,6 +4,7 @@ include SessionsHelper
 
 RSpec.describe TopicsController, type: :controller do
   let (:my_topic) { create(:topic) }
+  let (:my_private_topic) { create(:topic, public: false) }
 
   context "guest" do
     describe "GET index" do
@@ -19,6 +20,11 @@ RSpec.describe TopicsController, type: :controller do
     end
 
     describe "GET show" do
+      it "redirects from private topics" do
+        get :show, {id: my_private_topic.id}
+        expect(response).to redirect_to(new_session_path)
+      end
+      
       it "returns http success" do
         get :show, {id: my_topic.id}
         expect(response).to have_http_status(:success)
@@ -72,6 +78,11 @@ RSpec.describe TopicsController, type: :controller do
         expect(response).to redirect_to(new_session_path)
       end
     end
+
+    it "does not include private topics in @topics" do
+      get :index
+      expect(assigns(:topics)).not_to include(my_private_topic)
+    end
   end
 
   context "member user" do
@@ -86,9 +97,9 @@ RSpec.describe TopicsController, type: :controller do
         expect(response).to have_http_status(:success)
       end
 
-      it "assigns my_topic to @topics" do
+      it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
       end
     end
 
@@ -162,7 +173,7 @@ RSpec.describe TopicsController, type: :controller do
 
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
       end
     end
 
